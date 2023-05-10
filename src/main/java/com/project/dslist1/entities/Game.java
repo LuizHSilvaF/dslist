9f48fd8c259af0ac86b0d4489bca3d3bfc6713db
LuizHSilvaF <luiz.henriqueoff10@gmail.com>
2023-05-10 13:44:47 -0300

    game seed

--- src/main/java/com/project/dslist1/entities/Game.java
+++ src/main/java/com/project/dslist1/entities/Game.java
@@ -1,6 +1,5 @@
 package com.project.dslist1.entities;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 import jakarta.persistence.Column;
@@ -12,37 +11,40 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_game")
-public class Game implements Serializable{
-	private static final long serialVersionUID = 1L;
+public class Game {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
 	private String title;
-	
+
 	@Column(name = "game_year")
 	private Integer year;
-	
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
-	private String shortDescription;
-	private String longDescription;
-	
-	public Game() {
-	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
+
+    public Game() {
+    }
+
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+		this.longDescription = longDescription;		
 	}
 
 	public Long getId() {
@@ -77,12 +79,20 @@ public class Game implements Serializable{
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -125,6 +135,4 @@ public class Game implements Serializable{
 		Game other = (Game) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
 }
